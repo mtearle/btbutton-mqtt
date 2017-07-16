2548a08145599169a9ca342f5c77c9001a2c93d5
Mark Tearle <mark@tearle.com>
2017-07-16 14:59:17 +0000

    Initial hack to remove lifx code

--- lifx-remote.py
+++ lifx-remote.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python2
 
 import evdev
-import lifx
+#import lifx
 import time
 
-COLOR_NORMAL = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=1.0, kelvin=3500)
-COLOR_NIGHT = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=0.1099870298313878, kelvin=2750)
+#COLOR_NORMAL = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=1.0, kelvin=3500)
+#COLOR_NIGHT = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=0.1099870298313878, kelvin=2750)
 
 
 def button(lights, color):
@@ -22,32 +22,37 @@ def button(lights, color):
 
 def big_button(lights):
     print "big button pressed"
-    button(lights, COLOR_NORMAL)
+#    button(lights, COLOR_NORMAL)
 
 def small_button(lights):
     print "small button pressed"
-    button(lights, COLOR_NIGHT)
+#    button(lights, COLOR_NIGHT)
 
 def main(argv):
     device = evdev.InputDevice(argv[1])
     device.grab()
-    lights = lifx.Client()
-    enter_pressed = False
+#    lights = lifx.Client()
+    lights = "FOO"
+#    enter_pressed = False
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             event = evdev.categorize(event)
             key_up = event.keystate == evdev.KeyEvent.key_up
             vol_up = event.keycode == 'KEY_VOLUMEUP'
             enter = event.keycode == 'KEY_ENTER'
+	    print event.keystate
             if key_up and enter:
-                enter_pressed = True
+#                enter_pressed = True
+                small_button(lights)
             elif key_up and vol_up:
-                lights.discover()
-                if enter_pressed:
-                    small_button(lights)
-                else:
-                    big_button(lights)
-                enter_pressed = False
+                #lights.discover()
+                #if enter_pressed:
+                #    small_button(lights)
+                #else:
+                #    big_button(lights)
+                #enter_pressed = False
+                big_button(lights)
+            print "..."
 
 def usage():
    print("usage: lifx-remote.py /dev/input/event?")
