41414ceff27e114a7517df14115e98ec0ad2e576
Mark Tearle <mark@tearle.com>
2017-07-16 15:46:29 +0000

    Initial hacking ready to integrate mqtt

--- lifx-remote.py
+++ lifx-remote.py
@@ -1,39 +1,22 @@
 #! /usr/bin/python2
 
 import evdev
-#import lifx
-import time
 
-#COLOR_NORMAL = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=1.0, kelvin=3500)
-#COLOR_NIGHT = lifx.color.HSBK(hue=0.0, saturation=0.0, brightness=0.1099870298313878, kelvin=2750)
+def button(device, id, key):
+    print "device %s id %s key %s" % (device,id,key)
 
+def big_button(device, id):
+    button(device, id, "big")
 
-def button(lights, color):
-    devices = lights.get_devices()
-    if len(devices) > 0:
-        if not devices[0].power or devices[0].color != color:
-            for device in devices:
-                device.color = color
-                time.sleep(0.1)
-                device.power = True
-        else:
-            for device in devices:
-                device.power = False
+def small_button(device, id):
+    button(device, id, "small")
 
-def big_button(lights):
-    print "big button pressed"
-#    button(lights, COLOR_NORMAL)
-
-def small_button(lights):
-    print "small button pressed"
-#    button(lights, COLOR_NIGHT)
-
-def main(argv):
-    device = evdev.InputDevice(argv[1])
+def main(path,description):
+    device = evdev.InputDevice(path)
+    id = description
     device.grab()
-#    lights = lifx.Client()
-    lights = "FOO"
-#    enter_pressed = False
+    print device
+    print device.info
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             event = evdev.categorize(event)
@@ -42,16 +25,9 @@ def main(argv):
             enter = event.keycode == 'KEY_ENTER'
 	    print event.keystate
             if key_up and enter:
-#                enter_pressed = True
-                small_button(lights)
+                small_button(device.phys,id)
             elif key_up and vol_up:
-                #lights.discover()
-                #if enter_pressed:
-                #    small_button(lights)
-                #else:
-                #    big_button(lights)
-                #enter_pressed = False
-                big_button(lights)
+                big_button(device.phys,id)
             print "..."
 
 def usage():
@@ -60,6 +36,6 @@ def usage():
 if __name__ == '__main__':
    import sys
    if len(sys.argv) > 1:
-       main(sys.argv)
+       main(sys.argv[1],sys.argv[2])
    else:
       usage()
